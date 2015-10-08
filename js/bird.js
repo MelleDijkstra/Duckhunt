52e35cdf56405bc418bf5dbba9220a2838d2b620
Melletjee <melle210202@gmail.com>
2015-10-08 11:23:57 +0200

    Edited some stuff css js html

--- js/bird.js
+++ js/bird.js
@@ -2,12 +2,15 @@
  * Created by melle on 2-10-2015.
  */
 var bird = {
-    blueprint: '<img src="../images/duck.gif" alt="duck" />',
+    blueprint: '<img class="bird" src="images/duck.gif" alt="duck" />',
     create: function() {
         var element = $(this.blueprint);
         element.css('left','0px');
         element.css('top',getRandNum(20,400));
         $('main').append(element);
         log("Bird created");
+    },
+    fly: function() {
+        
     }
 };
---
+++ js/game.js
@@ -0,0 +1,6 @@
+/**
+ * Created by melle_000 on 2015-10-08.
+ */
+var game = {
+
+};
